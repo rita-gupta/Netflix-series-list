1e54c0e627ba844cc0d9354c25637ee525c9521c
rita gupta <ritagupta1014@gmail.com>
2020-08-18 16:27:15 +0545

    array method is used to render components

---
+++ src/Sdata.js
@@ -0,0 +1,35 @@
+const Sdata = [
+    {
+        imgsrc:"https://picsum.photos/200/300",
+        sname:"A Netflix Original series",
+        title:"LadyBird",
+        link: "https://www.netflix.com/watch/80205227?trackId=13752289&tctx=0%2C0%2C3c594b11d2a4ba4c204c03ff771e4e655a1a295e%3Ae8fed55d37e6a71dece5776f3ba1048f2855d5be%2C3c594b11d2a4ba4c204c03ff771e4e655a1a295e%3Ae8fed55d37e6a71dece5776f3ba1048f2855d5be%2C%2C" 
+        
+    },
+
+    {
+        imgsrc:"https://picsum.photos/200/200",
+        sname:"A Netflix Original series",
+        title:"Bulbul",
+        link:"https://www.netflix.com/watch/81029150?trackId=13752289&tctx=0%2C2%2C5b1c2eefe92a634fe6d7f9b2b67cd8da6e95465e%3Ac863d4fd2b05f78973f8416c28b1ea994756d16b%2C5b1c2eefe92a634fe6d7f9b2b67cd8da6e95465e%3Ac863d4fd2b05f78973f8416c28b1ea994756d16b%2C%2C"
+        
+    },
+
+    {
+        imgsrc:"https://picsum.photos/200/100",
+        sname:"A Netflix Original series",
+        title:"Bhulbhulaiya",
+        link:"https://www.netflix.com/watch/70082633?trackId=13752289&tctx=0%2C10%2C5b1c2eefe92a634fe6d7f9b2b67cd8da6e95465e%3Ac863d4fd2b05f78973f8416c28b1ea994756d16b%2C5b1c2eefe92a634fe6d7f9b2b67cd8da6e95465e%3Ac863d4fd2b05f78973f8416c28b1ea994756d16b%2C%2C"
+        
+    },
+
+    {
+        imgsrc:"https://picsum.photos/200/300",
+        sname:"A Netflix Original series",
+        title:"Queen",
+        link:"https://www.netflix.com/watch/80032081?trackId=13752289&tctx=0%2C18%2C5b1c2eefe92a634fe6d7f9b2b67cd8da6e95465e%3Ac863d4fd2b05f78973f8416c28b1ea994756d16b%2C5b1c2eefe92a634fe6d7f9b2b67cd8da6e95465e%3Ac863d4fd2b05f78973f8416c28b1ea994756d16b%2C%2C"
+        
+    }
+]
+
+export default Sdata;
